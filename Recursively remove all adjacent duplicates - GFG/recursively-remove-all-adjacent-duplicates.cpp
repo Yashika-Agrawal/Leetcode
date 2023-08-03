3d9cea6f82d7de061b6d0c82cf1dcb50e3201f02
Yashika Agrawal <97408966+Yashika-Agrawal@users.noreply.github.com>
2023-08-03 22:46:35 +0530

    Added solution - LeetHub

---
+++ Recursively remove all adjacent duplicates - GFG/recursively-remove-all-adjacent-duplicates.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    string rremove(string s){
+        while (1)
+    {
+        int i=0, n=s.size();
+        string temp="";
+        while(i<n)
+        {
+            int j=i+1;
+            while(j<n && s[i]==s[j]) j++;
+            if(j==i+1) temp+=s[i];
+            i=j;
+        }
+        if(s.size()==temp.size()) break;
+        s=temp;
+    }
+    return s;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Solution ob;
+        cout << ob.rremove(s) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
