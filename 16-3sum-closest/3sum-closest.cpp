d2e5e96960690bc78c621a1963af6dfa4e66096b
Yashika Agrawal <97408966+Yashika-Agrawal@users.noreply.github.com>
2026-09-05 22:07:36 +0530

    Time: 5 ms (91.70%) | Memory: 14 MB (50.40%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& a, int target) {
+        int sum=0;
+        int n=a.size();
+        int mini=INT_MAX;
+        int sub=0, ans=0;
+        sort(a.begin(), a.end());
+        for(int i=0;i<n-2;i++)
+        {
+            int j=i+1, k=n-1;
+            sum=a[i]+a[j]+a[k];
+            while(j<k)
+            {
+                sum=a[i]+a[j]+a[k];
+                if(sum==target)
+                {
+                    return sum;
+                }
+                else if(sum<target)
+                {
+                    sub=abs(target-sum);
+                    if(mini>sub)
+                    {
+                        mini=sub;
+                        ans=sum;
+                    }
+                    j++;
+                }
+                else{
+                    sub=abs(target-sum);
+                    if(mini>sub)
+                    {
+                        mini=sub;
+                        ans=sum;
+                    }
+                     k--;
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
